187694c0f718310b3a675957fa127d6daaf4774b
tak0kada <okokada.takuya+github@gmail.com>
2017-01-14 17:18:13 +0900

    Create 436.cpp

---
+++ yukicoder/436.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int solve(string input)
+{
+    int pos_first = input.find("ccw");
+    int pos_last = input.rfind("ccw");
+    int length = input.length();
+    
+    if (pos_first > length)
+        return 0;
+    else
+        return min(pos_last + 1, length - pos_first - 2);
+}
+
+int main(void)
+{
+    string input;
+    cin >> input;
+    cout << solve(input) << endl;
+}
